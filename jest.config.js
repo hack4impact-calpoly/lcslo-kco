9a30aef99210aa2fe56d77d08158b53cfbf0b0bd
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-01-12 19:30:04 -0800

    fix: fixed error with jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   preset: "ts-jest", // Use ts-jest for TypeScript support
-  testEnvironment: "jsdom", // For testing React components (use jsdom as the environment)
+  testEnvironment: "jest-environment-jsdom", //use jsdom as the environment)
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy", // If you're using CSS modules
   },
   transform: {
     "^.+\\.tsx?$": "ts-jest", // Transpile .ts and .tsx files
   },
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect", // Setup for jest-dom matchers
-  ],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
