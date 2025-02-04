fc0909a768762d971b2c8e4bbeba6e06a5f93ee4
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-02-03 21:43:55 -0800

    feat: added get request to get all audios and a post method, specified collection name

--- src/database/models/audiofileSchema.ts
+++ src/database/models/audiofileSchema.ts
@@ -1,10 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
-const audiofileSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  url: { type: String, required: true, unique: true },
-  duration: { type: String, required: true },
-  description: { type: String, required: true },
-});
+const audiofileSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    url: { type: String, required: true, unique: true },
+    duration: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { collection: "audiofiles" },
+);
 
 export default mongoose.models.Audiofile || mongoose.model("Audiofile", audiofileSchema);
