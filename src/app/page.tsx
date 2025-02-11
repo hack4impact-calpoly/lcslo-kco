de4caf253ca49f6789607c456765d4fc0fb875c8
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2025-02-10 19:09:13 -0800

    audio stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@/components/Navbar";
+import AudioPlayer from "@/components/AudioPlayer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1> {/* sample audio formatting */}
+      <AudioPlayer audioURL="https://www.example.com/sample-audio.mp3" name="Sample Audio" />
     </main>
   );
+  s;
 }
