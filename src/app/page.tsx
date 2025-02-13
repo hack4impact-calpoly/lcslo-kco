9a7d474662aa7be381bb974dfbf2826e4ba70496
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-02-13 15:53:02 -0800

    added POI list to home page for testing, could not see though

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/Navbar";
+import POIListView from "@/components/poiList";
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <POIListView />
     </main>
   );
 }
