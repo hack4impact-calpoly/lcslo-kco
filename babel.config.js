a80d98e4e62e66a204d22829758b0f713bfb9750
tammy-si <sitammy6@gmail.com>
2025-03-01 17:40:46 -0800

    test: setting up configs for testing and wrote first POICard test

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-react", // Adds support for JSX,
+    "@babel/preset-typescript", // For TypeScript support (removes type checking)
+  ],
+
+  plugins: [],
+};
