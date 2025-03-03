5656a1b0870a4733dc424f21079eff2dccacde1f
tammy-si <sitammy6@gmail.com>
2025-03-02 19:35:23 -0800

    update

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 import { TextEncoder, TextDecoder } from "util";
-import "dotenv/config"; // This will automatically load variables from your .env file
+import "dotenv/config"; // This will automatically load variables from .env file
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
