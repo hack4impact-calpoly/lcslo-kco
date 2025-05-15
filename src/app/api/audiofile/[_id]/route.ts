7e3a87e734614d8a21364bd23d627a833e6440e2
emily-yarvis <eyarvis@calpoly.edu>
2025-05-14 20:45:48 -0700

    Fixing deployment issues

--- src/app/api/audiofile/[_id]/route.ts
+++ src/app/api/audiofile/[_id]/route.ts
@@ -15,44 +15,34 @@ async function ensureDatabaseConnection() {
 }
 
 // Get specific audiofile by ID
-export async function GET(request: NextRequest, { params }: { params: { _id: string } }) {
-  console.log("PARAMS", params);
-  const { _id } = params;
+export async function GET(request: NextRequest, { params }) {
+  const _id = params._id as string; // TS will infer params is Record<string,string|string[]>
 
   try {
     await ensureDatabaseConnection();
-    console.log("GETTING AUDIOFILE", _id);
     const audiofile = await Audiofile.findById(_id).exec();
-    console.log(audiofile);
     if (!audiofile) {
       return NextResponse.json({ error: "Audiofile not found." }, { status: 404 });
     }
     return NextResponse.json(audiofile, { status: 200 });
   } catch (error) {
     console.error("Error fetching audiofile:", error);
-
-    // Handle Mongoose CastError (invalid ObjectId)
     if (error instanceof mongoose.Error.CastError) {
       return NextResponse.json({ error: "Invalid audiofile ID." }, { status: 400 });
     }
-
-    // Handle other errors
     return NextResponse.json({ error: "Error: Unable to fetch the audiofile." }, { status: 500 });
   }
 }
 
 // Update specific audiofile by ID
-export async function PUT(request: NextRequest, { params }: { params: { _id: string } }) {
-  const { _id } = params;
+export async function PUT(request: NextRequest, { params }) {
+  const _id = params._id as string;
 
   try {
     await ensureDatabaseConnection();
 
-    // Parse the request body
-    const body = await request.json();
-    const { name, duration, description } = body;
+    const { name, duration, description } = await request.json();
 
-    // Find the audiofile by ID and update it
     const updatedAudiofile = await Audiofile.findByIdAndUpdate(
       _id,
       { $set: { name, duration, description } },
@@ -66,13 +56,9 @@ export async function PUT(request: NextRequest, { params }: { params: { _id: str
     return NextResponse.json(updatedAudiofile, { status: 200 });
   } catch (error) {
     console.error("Error updating audiofile:", error);
-
-    // Handle Mongoose CastError (invalid ObjectId)
     if (error instanceof mongoose.Error.CastError) {
       return NextResponse.json({ error: "Invalid audiofile ID." }, { status: 400 });
     }
-
-    // Handle other errors
     return NextResponse.json({ error: "Error: Unable to update the audiofile." }, { status: 500 });
   }
 }
